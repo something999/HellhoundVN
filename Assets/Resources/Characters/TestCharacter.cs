1ff070ccb20f0486d39260764708404eb86e6d3b
something999 <wongny1@uci.edu>
2020-12-11 14:59:29 -0800

    Added folders, a base character class, and two debug objects for future testing purposes.

---
+++ Assets/Resources/Characters/TestCharacter.cs
@@ -0,0 +1,16 @@
+﻿// A test character used for debugging purposes.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacter : Character
+{
+    private void Start()
+    {
+        name = "Debug Helper";
+        paths_to_images = new Dictionary<string, string>() {
+            {"neutral", "Sprites/Debug/Debug.png"},
+            {"happy", "Sprites/Debug/Debug.png"}
+            };
+    }
+}
